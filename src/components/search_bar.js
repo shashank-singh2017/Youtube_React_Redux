68506cc54b671cf4cc90791ea4720f138a4a6469
Shashank Singh <shashank.singh@sjsu.edu>
2017-10-18 22:36:59 -0700

    Added video list item component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,17 +1,22 @@
 import React, { Component} from 'react';
 
 class SearchBar extends Component {
+
+  constructor(props){
+  	super(props);
+  	this.state = {term: ''};
+  }
+
+
   render() {
     return (
       <div>
-        <input onChange={this.onInputChange}/>
+        <input
+        value={this.state.term}
+        onChange={event => console.log(event.target.value)}/>
       </div>
     );
   }
-
-onInputChange(event) {
-  console.log(event.target.value);
-}
 
 }
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 // importing SearchBar from search_bar.js with proper file path name
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCRmwPMQ8pDAXwBSzV7G75vy_YOq_-z-1M';
 
+class App extends Component {
 
-const App = function () {
+  constructor(props){
+  	super(props);
+  	this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'shrey singhal'},  (videos) => {
+        this.setState({videos: videos})
+    });
+
+  }
+render() {
   return (
     <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} /> {/*method of passing data from parent(App) to child class (VideoList).*/}
     </div>
   );
+}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
